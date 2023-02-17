01ec228b82282e58ff6a395c5f0b0d83e9180f51
percy <percymichael0@gmail.com>
2023-02-17 20:25:44 +0300

    socials form

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,12 +1,30 @@
+import Head from "next/head";
+import Header from "./Header";
 import SideBarRow from "./SideBarRow";
 
 const SideBar = () => {
   return (
-    <div className=" w-2/4">
-      <div className="bg-[#FAFAFA] w-full py-6 rounded-2xl border border-[#F4F4F5]">
-        <SideBarRow name="Profile" />
-        <SideBarRow name="Profile" />
-      </div>
+    <div className="min-h-screen max-w-screen-lg mx-auto">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+
+      <main className="flex flex-row w-full py-10 gap-10">
+        <SideBar />
+
+        <ProfileForm />
+      </main>
+
+      <footer className="flex h-24 w-full items-center justify-center border-t">
+        <a
+          className="flex items-center justify-center gap-2"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        ></a>
+      </footer>
     </div>
   );
 };
--- components/VisibilityOption.tsx
+++ components/VisibilityOption.tsx
@@ -14,17 +14,7 @@ const VisibilityOption = (props: SideBarRowProps) => {
         </p>
       </div>
       <div className="">
-        <div className="flex items-center justify-center w-full">
-          <label htmlFor="toggleB" className="flex items-center cursor-pointer">
-            <div className="relative">
-              <input type="checkbox" id="toggleB" className="sr-only" />
-
-              <div className="block bg-[#4F46E5] w-12 h-7 rounded-full"></div>
-
-              <div className="dot absolute left-1 top-1 bg-white w-5 h-5 rounded-full transition"></div>
-            </div>
-          </label>
-        </div>
+        <input type="checkbox" className="toggle toggle-primary" />
       </div>
     </div>
   );
